494e65c08fcaddfc671f94b8d084b2f14aebb6d1
Akash Ganesh <akashganesh2005@gmail.com>
2025-08-31 15:21:47 +0530

    created normal and priority encoder

--- encoder/src/encoder.v
+++ encoder/src/encoder.v
@@ -1,5 +1,6 @@
 module encoder #(
-		parameter N = 2
+		parameter N = 4,
+		parameter PRIORITY_ENCODER = 0
 	)
 	(
 		input en,
@@ -10,10 +11,17 @@ module encoder #(
 	always @(*) begin 
 		if (en)
 			op = 0;
-		else 
-			for (i=0; i<N; i = i+1) begin
-				if (a[i])
-					op = i;
+		else begin
+			op = 0;
+			for (i=N-1; i>=0; i = i-1) begin
+				if (a[i]) begin
+					if (PRIORITY_ENCODER) begin
+						op = i;
+					end
+					else
+						op = op | i;
+				end
 			end
+		end
 	end
 endmodule
--- encoder/tb/tb_encoder.v
+++ encoder/tb/tb_encoder.v
@@ -1,7 +1,8 @@
 `timescale 1ns/1ps
 
 module tb #(
-	parameter N = 4
+	parameter N = 4,
+	parameter P = 1
 	) ();
 	localparam OP_SIZE = $clog2(N);
 
@@ -10,7 +11,8 @@ module tb #(
 	wire [OP_SIZE-1:0] op;
 
 	encoder #(
-		.N(N)
+		.N(N),
+		.PRIORITY_ENCODER(P)
 	) dut (
 		.en(en),
 		.a(a),
@@ -32,6 +34,11 @@ module tb #(
 		for (j=0; j<N; j = j+1) begin
 			a = 0;
 			a[j] = 1'b1;
+			#10;
+		end
+		en = 0;
+		for (j=0; j<(1<<N); j = j+1) begin
+			a = j;
 			#10;
 		end
 		$finish;
